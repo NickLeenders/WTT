b4c6de20833db7b284812680d16ae300229e5547
nickleenders <nicklndrs@hotmail.com>
2019-09-09 12:16:39 +0200

    Fixed excel read and added excel table instead of csv

--- main_assignment1.py
+++ main_assignment1.py
@@ -8,7 +8,7 @@ class Values:
         V_wind_in = 4 # Cut in wind speed in m/s
         V_wind_in = 25  # Cut out wind speed in m/s
         rho = 1.225 # air density in kg/m^3
-        blade_table = pd.read_csv("Assignment1.csv")
+        blade_table = pd.read_excel("Assignment1.xlsx")
         FFA_W3_241 = np.loadtxt('FFA-W3-241.txt')
         FFA_W3_301 = np.loadtxt('FFA-W3-301.txt')
         FFA_W3_360 = np.loadtxt('FFA-W3-360.txt')
