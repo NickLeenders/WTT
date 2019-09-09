2f48cdbfb79e223394fbddbfb14e362d434b1a87
andreas.thorsen <dre.thorsen@gmail.com>
2019-09-09 14:35:04 +0200

    Update loop_test.py

--- loop_test.py
+++ loop_test.py
@@ -13,32 +13,38 @@ Cd = 0.01
 B =  3
 
 #Initialize alpha
-a = 0; a_prime = 0;
+a = 0; a_prime = 0; a_crit = 1/3
 
 tol = 1E-6
+count = 0
 for i in range(len(r)):
-    while (abs(an-a)>tol):
+    while True:
+        count +=1
         phi = np.arctan(((1-a)*V0)/((1+a_prime)*omega*r[i]))
         alpha = phi-(beta+theta_p)
         
         #Table lookup for Cl, Cd
-        
         Cn = Cl*np.cos(phi) + Cd*np.sin(phi)
-        #F = (2/np.pi*np.arccos((np.exp((-B/2)*(R-r[i])/(r[i]*np.sin(abs(phi))))))
+        Ct = Cl*np.sin(phi) - Cd*np.cos(phi)
         
+        F = 2/np.pi*np.arccos(np.exp((-B/2)*(R-r[i])/(r[i]*np.sin(abs(phi)))))
+        #F = 0.981
         
-        F = 0.981
-        sigma = c*B/(2*np.pi*r)
-        a = 1/(4*F*(np.sin(phi)**2)/(sigma*Cn)-1)
-        
-        
-        
-        if a <= 1/3:
+        sigma = c*B/(2*np.pi*r[i])
+        #a = 1/(4*F*(np.sin(phi)**2)/(sigma*Cn)-1)
+           
+        if a <= a_crit:
             anew = 1/(4*F*(np.sin(phi)**2)/(sigma*Cn)-1)
         else:
-            CT = ((1-a)**2)*sigma*Cn/(np.sin(phi)**2)
-        
-        
+            K = 4*F*(np.sin(phi)**2)/(sigma*Cn)
+            anew = 1/2*(2+K*(1-2*a_crit)-np.sqrt((K*(1-2*a_crit)+2)**2+4*(K*a_crit**2-1)))
+        a_prime = 1/(4*F*(np.sin(phi)*np.cos(phi))/(sigma*Ct)-1)
+        if abs(anew-a) <= tol:
+            a = anew
+            break
+        else:
+            a = anew 
+
         
 
 
