e9c4ba0e533913375042ff24a727ad7e07ce3e7f
nickleenders <nicklndrs@hotmail.com>
2019-11-03 13:24:17 +0100

    Update Assignment2_1.py

--- Assignment2_1.py
+++ Assignment2_1.py
@@ -24,14 +24,39 @@ R_cable = 1 #Unit Ohm
 L_cable = 5*10**-3 #Unit H
 C_cable = 1*10**-6 #Unit F
 
+Xl_cable = L_cable*omegaGrid #Xl for cable connection
+Xc_cable = -1/(omegaGrid*C_cable) # Xc for cable connection
+
 # Circuit 1
 Vpoc = 33*10**3*np.sqrt(3)
 omegaGrid = 2*np.pi*50
 Z1 = R1 + L1*1j*omegaGrid
 Z2 = R1 + L1*1j*omegaGrid
-Z_circuit1 = Z1+Z2+R_cable+L_cable*omegaGrid*1j
+Z_transformer = Z1+Z2
+Z_cable = omegaGrid*C_cable
+Z_circuit1 = Z1+Z2+R_cable+Xl_cable*1j
 Ipoc = Ig
+
+# Circuit 2
+Z_circuit2 = Z1+Z2+R_cable+(Xl_cable+Xc_cable)*1j
 
 # Cable
 Ic = Vpoc/(1j/(omegaGrid*C_cable))+Ipoc
 Vc = Ic*(R_cable+1j*omegaGrid*L_cable)+Vpoc
+
+P2 = Ic.real*Vc.real
+Q2 = abs(Ic.imag*Vc.imag)
+
+# Transformer
+Vlow = 690 #unit V
+Vhigh = 33*10**3 #unit V
+N_ratio = Vlow/Vhigh # (N1/N2) no unit
+Vt = Vc*N_ratio
+It = Ic*N_ratio
+
+
+plt.plot(RPM,abs(P2))
+plt.plot(RPM,abs(Q2))
+plt.xlabel('n (RPM)')
+plt.ylabel('Power (W)')
+plt.legend(['Active power','Reactive power'])
